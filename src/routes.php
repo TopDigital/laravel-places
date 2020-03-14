76d225473fa03b9ff8b866338f20deed8eb0e9fe
Sobolev Sergey <soberzerg@gmail.com>
2020-03-14 18:55:30 +0300

    fix routes

--- src/routes.php
+++ src/routes.php
@@ -3,12 +3,8 @@
 Route::group([
     'namespace' => 'TopDigital\Content\Http\Controllers',
     'middleware' => ['api', 'cors', 'auth:api'],
-    'prefix' => 'api/content'
+    'prefix' => 'api'
 ], function () {
-
-    Route::resource('posts', 'PostsController')->only(['index', 'show', 'store', 'update', 'destroy']);
-    Route::post('posts/{post}/preview', 'PostsController@storePreview');
-
-    Route::resource('sections', 'SectionsController')->only(['index', 'store', 'update', 'destroy']);
+    Route::resource('places', 'PlacesController')->only(['index', 'store', 'update', 'destroy']);
 });
 
